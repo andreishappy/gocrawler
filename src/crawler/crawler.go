b6c28111fce3fec324971cba9e7db76ce55f435c
Andrei Petric <andrei@theappbusiness.com>
2017-02-21 08:39:41 +0000

    Crawler implemented with mutex and wait group, probably needs more tests

--- src/crawler/crawler.go
+++ src/crawler/crawler.go
@@ -1,36 +1,75 @@
 package crawler
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-type Page struct {
+type FetchedPage struct {
 	Links  []string
 	Assets []string
 }
 
 type PageNode struct {
-	Url string
-	Links []PageNode
+	Links []string
 	Assets []string
 }
 
 type Feeder interface {
-  getLinks(url string) Page
+  getLinks(url string) (error, *FetchedPage)
 }
 
 type Crawl struct {
 	feeder Feeder
 }
 
-func (c *Crawl) crawl(url string) PageNode {
+func (c *Crawl) crawl(url string) map[string]PageNode {
+	wg := sync.WaitGroup{}
+	mutex := sync.Mutex{}
+	visited := map[string]bool{}
+	result := map[string]PageNode{}
 
-	feederResult := c.feeder.getLinks(url)
-	fmt.Println(feederResult)
-	return PageNode{Url: url}
+	var crawlFunc func(string, map[string]bool, *Crawl)
+	crawlFunc = func(url string, visited map[string]bool, c *Crawl) {
+		defer wg.Done()
 
-	// create a set of visited urls
-	// instantiate a graph
+		//get the links and assets
+		e, page := c.feeder.getLinks(url)
 
-	// issue go routines that check if a url has already been visited
-	//   - if it has stop
-	//   -
+		if (e != nil) {
+			fmt.Println("Error when fetching " + url)
+			return
+		}
+
+		mutex.Lock()
+
+		//Check if url has already been visited
+		_, ok := visited[url]
+		if (ok) {
+			fmt.Println("Already saw ", url)
+			return
+		} else {
+			fmt.Println("Saw ", url, " for the first time")
+
+			//Spawn off go routines for the links
+			for _, link := range page.Links {
+				wg.Add(1)
+				fmt.Println("spawning go routine for ", link)
+				go crawlFunc(link, visited, c)
+				fmt.Println("after spawn for ", link)
+			}
+
+			visited[url] = true
+			result[url] = PageNode{Links: page.Links, Assets: page.Assets}
+		}
+
+		mutex.Unlock()
+
+	}
+
+	wg.Add(1)
+	crawlFunc(url, visited, c)
+	wg.Wait()
+	fmt.Println(result)
+	return result
 }
--- src/crawler/crawler_test.go
+++ src/crawler/crawler_test.go
@@ -4,23 +4,71 @@ package crawler
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"errors"
+	"fmt"
 )
 
 type TestFeeder struct {
+	m map[string]*FetchedPage
 }
 
-var m = map[string]Page{
+var m = map[string]*FetchedPage{
 	"hello": {Links: []string{"yo"}, Assets: []string{}},
+	"yo": {Links: []string{"my"}, Assets: []string{}},
 }
 
-func (t TestFeeder) getLinks(url string) Page {
-	return m[url]
+var selfReference = map[string]*FetchedPage{
+	"hello": {Links: []string{"hello"}, Assets: []string{}},
 }
 
-func TestFirst(t *testing.T) {
+var circularReferences = map[string]*FetchedPage{
+	"hello": {Links: []string{"hi"}, Assets: []string{}},
+	"hi": {Links: []string{"hello"}, Assets: []string{}},
+}
 
-	f := TestFeeder{}
+var withAssets = map[string]*FetchedPage{
+	"hello": {Links: []string{"hi"}, Assets: []string{"helloAsset"}},
+	"hi": {Links: []string{"go"}, Assets: []string{"hiAsset1", "hiAsset2"}},
+}
 
+func (t TestFeeder) getLinks(url string) (error, *FetchedPage) {
+	elem, ok := t.m[url]
+	if (ok) {
+		return nil, elem
+	} else {
+		return errors.New(fmt.Sprint("Error when fetching ", url)), nil
+	}
+}
+
+func TestDoesNotHangWhenAPageReferencesItself(t *testing.T) {
+	f := TestFeeder{m: selfReference}
 	p := Crawl{f}
-	assert.Equal(t, p.crawl("hello"), PageNode{Url: "hello"}, "they should be equal")
+	assert.Equal(t, map[string]PageNode{"hello": {Links: []string{"hello"}, Assets: []string{}}}, p.crawl("hello"))
+}
+
+func TestDoesNotHangWhenA2PageCircularDependencyExists(t *testing.T) {
+	f := TestFeeder{m: circularReferences}
+	p := Crawl{f}
+	expected := map[string]PageNode{
+		"hello": {Links: []string{"hi"}, Assets: []string{}},
+		"hi": {Links: []string{"hello"}, Assets: []string{}},
+	}
+	assert.Equal(t, expected, p.crawl("hello"))
+}
+
+func TestAddPageWhenFetcherReturnsError(t *testing.T) {
+	f := TestFeeder{m: map[string]*FetchedPage{}}
+	p := Crawl{f}
+	expected := map[string]PageNode{}
+	assert.Equal(t, expected, p.crawl("hello"))
+}
+
+func TestAddsAssetsToPages(t *testing.T) {
+	f := TestFeeder{m: withAssets}
+	p := Crawl{f}
+	expected := map[string]PageNode{
+		"hello": {Links: []string{"hi"}, Assets: []string{"helloAsset"}},
+		"hi": {Links: []string{"go"}, Assets: []string{"hiAsset1", "hiAsset2"}},
+	}
+	assert.Equal(t, expected, p.crawl("hello"))
 }
